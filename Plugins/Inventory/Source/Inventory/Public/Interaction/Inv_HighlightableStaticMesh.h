911da597b260a5d35739f663bc9fdf2d020e3cd5
FoolAntony _ <mrcrazytony@gmail.com>
2025-06-07 13:25:41 +0200

    HighlightableStaticMesh component created

---
+++ Plugins/Inventory/Source/Inventory/Public/Interaction/Inv_HighlightableStaticMesh.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "Inv_HighlightableStaticMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_HighlightableStaticMesh : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+};
