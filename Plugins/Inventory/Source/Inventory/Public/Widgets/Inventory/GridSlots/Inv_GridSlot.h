42302cbb13e748ad25c652b0a87d7caf46d9d8ae
FoolAntony _ <mrcrazytony@gmail.com>
2025-06-10 11:47:35 +0200

    Grid generates with GridSlots as Canvas Panel

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/GridSlots/Inv_GridSlot.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_GridSlot.generated.h"
+
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_GridSlot : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	//====================
+	//	FUNCTIONS
+	//====================
+
+	void SetTileIndex(int32 Index) {TileIndex = Index;}
+	int32 GetTileIndex() const {return TileIndex;}
+
+private:
+	//=========================
+	//	PARAMETERS & VARIABLES
+	//=========================
+
+	int32 TileIndex;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_GridSlot;
+};
--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spacial/Inv_InventoryGrid.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spacial/Inv_InventoryGrid.h
@@ -7,6 +7,8 @@
 #include "Types/Inv_GridTypes.h"
 #include "Inv_InventoryGrid.generated.h"
 
+class UInv_GridSlot;
+class UCanvasPanel;
 /**
  * 
  */
@@ -21,14 +23,39 @@ public:
 	//====================
 	
 	EInv_ItemCategory GetItemCategory() const {return ItemCategory;}
+	virtual void NativeOnInitialized() override;
 	
 	
 
 private:
+	//====================
+	//	FUNCTIONS
+	//====================
+
+	void ConstructGrid();
+	
 	//=========================
 	//	PARAMETERS & VARIABLES
 	//=========================
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true") ,Category = "Inventory")
 	EInv_ItemCategory ItemCategory;
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	TSubclassOf<UInv_GridSlot> GridSlotClass;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCanvasPanel> CanvasPanel;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UInv_GridSlot>> GridSlots;
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	int32 Rows;
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	int32 Columns;
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	float TileSize;
 };
