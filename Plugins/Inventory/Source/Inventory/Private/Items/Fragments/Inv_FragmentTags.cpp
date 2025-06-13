5da3ca16ecc672adcbf054976870ce86692393af
FoolAntony _ <mrcrazytony@gmail.com>
2025-06-13 14:09:47 +0200

    Added StackableFragment for stackable items

--- Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
@@ -4,4 +4,5 @@ namespace FragmentTags
 {
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GridFragment, "FragmentTags.GridFragment", "GridFragment")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(IconFragment, "FragmentTags.IconFragment", "IconFragment")
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(StackableFragment, "FragmentTags.StackableFragment", "StackableFragment")
 }
