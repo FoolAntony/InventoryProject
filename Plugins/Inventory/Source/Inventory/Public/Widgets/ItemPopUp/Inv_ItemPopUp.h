bc6f634bafc56fb272ecacacf50d66530fcb0fa5
FoolAntony _ <mrcrazytony@gmail.com>
2025-06-18 13:11:55 +0200

    Added ItemPopUp class

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -0,0 +1,90 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_ItemPopUp.generated.h"
+
+/**
+ *  Shows up when right clicking on the item in the inventory grid
+ */
+
+class UButton;
+class USlider;
+class UTextBlock;
+class USizeBox;
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FPopUpMenuSplit, int32, SplitAmount, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuDrop, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuConsume, int32, Index);
+
+UCLASS()
+class INVENTORY_API UInv_ItemPopUp : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	//====================
+	//	FUNCTIONS
+	//====================
+
+	virtual void NativeOnInitialized() override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+
+	FPopUpMenuSplit OnSplit;
+	FPopUpMenuDrop OnDrop;
+	FPopUpMenuConsume OnConsume;
+
+	int32 GetSplitAmount() const;
+	int32 GetIndex() const { return GridIndex; };
+	void CollapseSplitButton() const;
+	void CollapseConsumeButton() const;
+	void SetSliderParams(const float Max, const float Value) const;
+	void SetGridIndex(const int32 Index) {GridIndex = Index;};
+	FVector2D GetBoxSize() const;
+
+	
+
+private:
+	//====================
+	//	FUNCTIONS
+	//====================
+
+	UFUNCTION()
+	void SplitButtonClicked();
+
+	UFUNCTION()
+	void DropButtonClicked();
+
+	UFUNCTION()
+	void ConsumeButtonClicked();
+
+	UFUNCTION()
+	void SliderValueChanged(float Value);
+
+	//=========================
+	//	PARAMETERS & VARIABLES
+	//=========================
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Split;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consume;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Drop;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> Slider_Split;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_SplitAmount;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Root;
+
+	int32 GridIndex{INDEX_NONE};
+	
+};
